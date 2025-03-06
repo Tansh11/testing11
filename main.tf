f3f0cc79e0ddee3686b83469f08b92f7cca64375
Tansh11 <137879984+Tansh11@users.noreply.github.com>
2025-03-06 13:04:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 # Fetch subscription details using the data source
 data "azurerm_subscription" "sub" {}
 
-# Create the resource group for the identity (can be omitted if already created)
+# Create the resource group for the identity
 module "identity-resource-group" {
   source   = "../modules/resource-group"
   name     = var.identity_rg_name
@@ -23,11 +23,24 @@ module "gh_usi" {
   tags     = var.tags
 }
 
-# Assign the Owner role to the user-assigned identity
-module "sub_owner_role_assignment" {
-  source       = "../modules/role-assignment"
-  principal_id = module.gh_usi.user_assinged_identity_principal_id
-  role_name    = "Owner"
-  principal_type = "ServicePrincipal"
-  scope_id     = data.azurerm_subscription.sub.id  # Using the subscription ID from the data source
+# Federated Identity Credential for Environment (e.g., dev)
+module "gh_federated_credential" {
+  source                             = "../modules/federated-identity-credential"
+  federated_identity_credential_name = "${var.github_organization_target}-${var.github_repository}-${var.environment}"
+  rg_name                            = module.identity-resource-group.name
+  user_assigned_identity_id          = module.gh_usi.user_assinged_identity_id
+  subject                            = "repo:${var.github_organization_target}/${var.github_repository}:environment:${var.environment}"
+  audience_name                      = local.default_audience_name
+  issuer_url                         = local.github_issuer_url
+}
+
+# Federated Identity Credential for Pull Requests
+module "gh_federated_credential-pr" {
+  source                             = "../modules/federated-identity-credential"
+  federated_identity_credential_name = "${var.github_organization_target}-${var.github_repository}-pr"
+  rg_name                            = module.identity-resource-group.name
+  user_assigned_identity_id          = module.gh_usi.user_assinged_identity_id
+  subject                            = "repo:${var.github_organization_target}/${var.github_repository}:pull_request"
+  audience_name                      = local.default_audience_name
+  issuer_url                         = local.github_issuer_url
 }
