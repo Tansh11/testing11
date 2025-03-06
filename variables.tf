24a6379a0a0b29b586612a6507796bd317a33dd0
Tansh11 <137879984+Tansh11@users.noreply.github.com>
2025-03-06 12:31:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
+variable "identity_rg_name" {
+  description = "The name of the resource group for the user-assigned identity."
+  type        = string
+}
+
+variable "location" {
+  description = "The location where resources will be created."
+  type        = string
+}
+
+variable "gh_uai_name" {
+  description = "The base name of the user-assigned identity."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment in which this resource is being created (e.g., dev, prod)."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be assigned to resources."
+  type        = map(string)
+}
 
