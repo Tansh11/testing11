fd05ccda0851913ce75d7919bb566ba06861d9bb
Tansh11 <137879984+Tansh11@users.noreply.github.com>
2025-03-06 12:31:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+module "identity-resource-group" {
+  source   = "../modules/resource-group"
+  name     = var.identity_rg_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "gh_usi" {
+  source   = "../modules/user-assigned-identity"
+  name     = "${var.gh_uai_name}-${var.environment}"
+  location = var.location
+  rg_name  = module.identity-resource-group.name
+  tags     = var.tags
+}
 
