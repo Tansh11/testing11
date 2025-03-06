85ccb9f67cd44c191c44bc488616d53497069dc0
Tansh11 <137879984+Tansh11@users.noreply.github.com>
2025-03-06 13:55:36 +0530

    Create variables.tf

---
+++ modules/resource-group/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The location where the resource group will be created."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to the resource group."
+  type        = map(string)
+}
