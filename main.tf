f643e4c84768630902b15874a3e7b06500482612
Tansh11 <137879984+Tansh11@users.noreply.github.com>
2025-03-06 12:48:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+
+provider "azurerm" {
+  features {}
+}
+
+# Retrieve the subscription ID
+data "azurerm_subscription" "sub" {}
+
+# Create the resource group for the identity (already assumed to exist or you could add it here)
 module "identity-resource-group" {
   source   = "../modules/resource-group"
   name     = var.identity_rg_name
@@ -5,11 +14,20 @@ module "identity-resource-group" {
   tags     = var.tags
 }
 
+# Create the user-assigned identity
 module "gh_usi" {
   source   = "../modules/user-assigned-identity"
-  name     = "${var.gh_uai_name}-${var.environment}"
+  name     = var.gh_uai_name
   location = var.location
   rg_name  = module.identity-resource-group.name
   tags     = var.tags
 }
 
+# Assign the Owner role to the user-assigned identity
+module "sub_owner_role_assignment" {
+  source       = "../modules/role-assignment"
+  principal_id = module.gh_usi.user_assinged_identity_principal_id
+  role_name    = "Owner"
+  principal_type = "ServicePrincipal"
+  scope_id     = data.azurerm_subscription.sub.id
+}
